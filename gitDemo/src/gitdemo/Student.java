ff099e1c7bb71633229d96ebbef9d747cebe0755
AbdulRahmanFatouhe <fatouhe@sheridancollege.ca>
2023-02-03 15:04:44 -0500

    Feb 3 2023 commit of StudList

---
+++ gitDemo/src/gitdemo/Student.java
@@ -0,0 +1,38 @@
+
+package gitdemo;
+
+/**
+ * hello everyone from Abdul
+ * @author Megha Patel 
+ */
+public class Student {
+    private int studId;
+    private String studName;
+
+    public Student() {
+    }
+    
+    public Student(int studId, String studName) {
+        this.studId = studId;
+        this.studName = studName;
+    }
+
+    public String getStudName() {
+        return studName;
+    }
+
+    public void setStudName(String studName) {
+        this.studName = studName;
+    }
+
+    public int getStudId() {
+        return studId;
+    }
+
+    public void setStudId(int studId) {
+        this.studId = studId;
+    }
+    
+    
+
+}
